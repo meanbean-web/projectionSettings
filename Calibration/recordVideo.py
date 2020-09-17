936ecb8c89cecfc5788e88d718de2cbce7d8f8ca
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-17 18:15:10 +0300

    first commit

--- Calibration/recordVideo.py
+++ Calibration/recordVideo.py
@@ -6,7 +6,7 @@ camera = picamera.PiCamera()
 camera.vflip = True
 #camera.start_preview()
 camera.framerate = 32
-camera.start_recording(‘calibration.h264’)
+camera.start_recording("calibration")
 camera.wait_recording(3000)
 camera.stop_recording()
 #camera.stop_preview()
