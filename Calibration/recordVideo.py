89ce5763c1a1e7a173f623e1a0ff2ba809562d9e
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-17 18:17:50 +0300

    first commit

--- Calibration/recordVideo.py
+++ Calibration/recordVideo.py
@@ -7,6 +7,6 @@ camera.vflip = True
 #camera.start_preview()
 camera.framerate = 32
 camera.start_recording("calibration.h264")
-camera.wait_recording(3000)
+camera.wait_recording(30)
 camera.stop_recording()
 #camera.stop_preview()
