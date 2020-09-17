724eb72a129f0ab4a2eed257c9be495f31744a33
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-17 15:59:29 +0300

    first commit

---
+++ Calibration/displayGridFullscreen.py
@@ -0,0 +1,9 @@
+import cv2
+import time
+
+img = cv2.imread('circleGrid.png')
+cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty ('screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow('screen', img)
+cv2.waitKey(1)
+time.sleep(120)
