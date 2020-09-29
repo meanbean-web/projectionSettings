7d2a798117b3d66e1824d18ec044f8d2a47026f7
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-29 16:17:08 +0300

    first commit

--- Calibration/real-time-calib-raspi.py
+++ Calibration/real-time-calib-raspi.py
@@ -29,7 +29,7 @@ imageSize = None # Determined at runtime
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array  # store each incoming image as a numpy array
 
-    # cv2.imshow("Frame", image)
+    cv2.imshow("Frame", image)
 
     # while (True):
 
