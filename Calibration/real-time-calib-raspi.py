727416b0c108220be207962f8877ed891f0413fd
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-30 17:39:04 +0300

    first commit

--- Calibration/real-time-calib-raspi.py
+++ Calibration/real-time-calib-raspi.py
@@ -9,6 +9,9 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(848, 480))
 camera.vflip = True
 
+img = cv2.imread('image displays/circleGrid.png')
+print(img)
+
 time.sleep(0.2)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -19,7 +22,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-    cv2.imshow("Frame", image)
+    cv2.imshow("Frame", img)
     key = cv2.waitKey(1) & 0xFF
 
     rawCapture.truncate(0)
