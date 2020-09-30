4350ca2ca7f247bb09490ace71de1e2efd41a5a3
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-30 17:53:52 +0300

    first commit

--- Calibration/real-time-calib-raspi.py
+++ Calibration/real-time-calib-raspi.py
@@ -16,13 +16,25 @@ time.sleep(0.2)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #detect chessboard in the images supplied by the pi camera
+
+
 
     #full screen frame
 
     cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-    cv2.imshow("Frame", img)
+    #you need to add a break key for this
+
+
+
+
+
+
+    cv2.imshow("Frame", gray)
     key = cv2.waitKey(1) & 0xFF
 
     rawCapture.truncate(0)
