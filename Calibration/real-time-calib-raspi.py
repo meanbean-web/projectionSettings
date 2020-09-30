6909318bd1d8b084343f36a9c613452b27de0c20
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-30 17:32:25 +0300

    first commit

--- Calibration/real-time-calib-raspi.py
+++ Calibration/real-time-calib-raspi.py
@@ -14,6 +14,11 @@ time.sleep(0.2)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
 
+    #full screen frame
+
+    cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
 
