96ea83a64ae317f469418f8449d5bd8dec716387
Andreea Bunica <andreeabunica1997@gmail.com>
2020-09-30 18:03:55 +0300

    first commit

--- Calibration/real-time-calib-raspi.py
+++ Calibration/real-time-calib-raspi.py
@@ -2,6 +2,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import numpy
 
 camera = PiCamera()
 camera.resolution = (848, 480)
@@ -14,11 +15,37 @@ print(img)
 
 time.sleep(0.2)
 
+imageSize = None # Determined at runtime
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     #detect chessboard in the images supplied by the pi camera
+
+    board, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), None)
+    if board == True:
+        # Add the points in 3D that we just discovered
+        objpoints.append(objp)
+
+        # Enhance corner accuracy with cornerSubPix
+        corners_acc = cv2.cornerSubPix(
+            image=gray,
+            corners=corners,
+            winSize=(11, 11),
+            zeroZone=(-1, -1),
+            criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30,
+                      0.001))  # Last parameter is about termination critera
+        imgpoints.append(corners_acc)
+
+        # If our image size is unknown, set it now
+        if not imageSize:
+            imageSize = gray.shape[::-1]
+
+        # Draw the corners to a new image to show whoever is performing the calibration
+        # that the board was properly detected
+        img = cv2.drawChessboardCorners(img, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), corners_acc,
+                                        board)
 
 
 
@@ -34,7 +61,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
 
-    cv2.imshow("Frame", gray)
+    cv2.imshow("Frame", img)
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
         cv2.destroyAllWindows()
