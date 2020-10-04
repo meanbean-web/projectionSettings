ecd587a79b6a60cc04d29822c3d576b0b9eadd09
Andreea Bunica <andreeabunica1997@gmail.com>
2020-10-04 19:53:18 +0300

    first commit

--- Camera-projector/cameraCalibration.py
+++ Camera-projector/cameraCalibration.py
@@ -1,22 +1,79 @@
 # calibrate camera, write values to yaml file
+import cv2
 
 
-
-def liveCalibrate(img, checkerboard):
+def calibration (frame, counter, CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT, objpoints, imgpoints, imageSize, objp):
     counter += 1
     img = frame.array
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # LIVE CAMERA CALIBRATION
-    boards = [] #number of boards in which a checkerboard has been detected
-    goodBoards = [] #number of boards with a reprojection error smaller than
-    maxCameraError = 0 # see how you get this guy
+    # DETECT CHESSBOARD / aruco marker
 
-    if maxCameraError < 0.5:
-        goodBoards = #mark as goodBoard and trigger counter
+    board, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), None)
+    if board:
+        # Add the points in 3D that we just discovered
+        objpoints.append(objp)
 
-    if goodBoards == 40: #when you collect 40 good boards / good chessboard views
-        cv2.calibrateCamera()
-        calibration = yaml.dump()
+        # Enhance corner accuracy with cornerSubPix
+        corners_acc = cv2.cornerSubPix(
+            image=gray,
+            corners=corners,
+            winSize=(11, 11),
+            zeroZone=(-1, -1),
+            criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30,
+                      0.001))  # Last parameter is about termination critera
+        imgpoints.append(corners_acc)
 
-    return cameraCalib
+        # If our image size is unknown, set it now
+        if not imageSize:
+            imageSize = gray.shape[::-1]
+
+        # Draw the corners to a new image to show whoever is performing the calibration
+        # that the board was properly detected
+        img = cv2.drawChessboardCorners(img, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), corners_acc,
+                                        board)
+
+    cv2.imshow("Frame", img)
+    key = cv2.waitKey(1) & 0xFF
+
+    rawCapture.truncate(0)
+
+    if key == ord("q"):
+        break
+
+    if counter < 40:
+        print(counter)
+
+    # CALIBRATE CAMERA
+    # if the number of retrieved images is 40, then calibrate the camera > you might want to add the first 40 frames to a folder
+
+    if counter == 40:
+        calibration, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(
+            objectPoints=objpoints,
+            imagePoints=imgpoints,
+            imageSize=imageSize,
+            cameraMatrix=None,
+            distCoeffs=None)
+
+        # Print matrix and distortion coefficient to the console
+
+        print(cameraMatrix)
+        print(distCoeffs)
+
+    # counter += 1
+    # img = frame.array
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #
+    # # LIVE CAMERA CALIBRATION
+    # boards = [] #number of boards in which a checkerboard has been detected
+    # goodBoards = [] #number of boards with a reprojection error smaller than
+    # maxCameraError = 0 # see how you get this guy
+    #
+    # if maxCameraError < 0.5:
+    #     goodBoards = #mark as goodBoard and trigger counter
+    #
+    # if goodBoards == 40: #when you collect 40 good boards / good chessboard views
+    #     cv2.calibrateCamera()
+    #     calibration = yaml.dump()
+    #
+    # return cameraCalib
--- Camera-projector/mainExecutable.py
+++ Camera-projector/mainExecutable.py
@@ -1,8 +1,9 @@
-import projectionControl
-import cameraCalibration
+from cameraCalibration import calibration
+from projectionControl import projectCircles
 import cv2
 import time
 from picamera import PiCamera
+import numpy
 import yaml
 
 
@@ -12,7 +13,6 @@ camera.resolution = (848, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(848, 480))
 camera.vflip = True
-
 time.sleep(0.2)
 
 #DECLARE CHESSBOARD PROPERTIES
@@ -33,22 +33,75 @@ objp[:,:2] = numpy.mgrid[0:CHESSBOARD_CORNERS_ROWCOUNT,0:CHESSBOARD_CORNERS_COLC
 # get live video input from pi camera, project detected chessboard
 counter = 0
 
-while frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    liveCalib(frame, chessboard_corners_rowcount, chessboard_corners_colcount)
-    if liveCalib ended:
-        print('calibration successful, moving to the next step')
+while camera.capture_continuous:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        counter += 1
+        img = frame.array
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    
-    # project detected chessboard, print reprojection error
-    img = cv2.drawChessboardCorners(image, patternSize=, corners=, patternWasFound=)
+        # DETECT CHESSBOARD / aruco marker
 
-    #check number of good frames
+        board, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT),
+                                                   None)
+        if board == True:
+            # Add the points in 3D that we just discovered
+            objpoints.append(objp)
 
-    #
+            # Enhance corner accuracy with cornerSubPix
+            corners_acc = cv2.cornerSubPix(
+                image=gray,
+                corners=corners,
+                winSize=(11, 11),
+                zeroZone=(-1, -1),
+                criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30,
+                          0.001))  # Last parameter is about termination critera
+            imgpoints.append(corners_acc)
 
-    if reprojectionError > value:
-        # call from projectionControl def projectCircleGrid
-        pattern = cv2.imread('image files/circlesGrid.png')
-        images = img, pattern
-        cv2.calcBackProject()
-    # call projectionControl
+            # If our image size is unknown, set it now
+            if not imageSize:
+                imageSize = gray.shape[::-1]
+
+            # Draw the corners to a new image to show whoever is performing the calibration
+            # that the board was properly detected
+            img = cv2.drawChessboardCorners(img, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT),
+                                            corners_acc,
+                                            board)
+
+            # DETECT RED CIRCLES FROM THE PROJECTOR
+            circles_grid_size = (4, 7)
+            ret, circles = cv2.findCirclesGrid(img, circles_grid_size, flags=cv2.CALIB_CB_SYMMETRIC_GRID)
+            img = cv2.drawChessboardCorners(img, circles_grid_size, circles, ret)
+        # preview checkerboard detection frame + vectorial orientation
+
+        cv2.imshow("Frame", img)
+        key = cv2.waitKey(1) & 0xFF
+
+        rawCapture.truncate(0)
+
+        if key == ord("q"):
+            break
+
+        if counter < 40:
+            print(counter)
+
+        # CALIBRATE CAMERA
+        # if the number of retrieved images is 40, then calibrate the camera > you might want to add the first 40 frames to a folder
+
+        if counter == 40:
+            calibration, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(
+                objectPoints=objpoints,
+                imagePoints=imgpoints,
+                imageSize=imageSize,
+                cameraMatrix=None,
+                distCoeffs=None)
+
+            # Print matrix and distortion coefficient to the console
+            print(cameraMatrix)
+            print(distCoeffs)
+
+    # if reprojectionError > value:
+    #     # call from projectionControl def projectCircleGrid
+    #     pattern = cv2.imread('image files/circlesGrid.png')
+    #     images = img, pattern
+    #     cv2.calcBackProject()
+    # # call projectionControl
