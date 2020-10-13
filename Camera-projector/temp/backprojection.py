68c27f98b34da47ccef4e3876f3c288ffed0136c
Andreea Bunica <andreeabunica1997@gmail.com>
2020-10-13 15:57:46 +0300

    first commit

--- Camera-projector/temp/backprojection.py
+++ Camera-projector/temp/backprojection.py
@@ -1,36 +1,20 @@
+# Standard imports
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
+# Read image
+im = cv2.imread("image files/circleGrid.png")
 
-#roi is the object or region of object we need to find
-roi = cv2.imread('image files/circle.png')
-hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-#target is the image we search in
-target = cv2.imread('image files/circleGrid.png')
-hsvt = cv2.cvtColor(target, cv2.COLOR_BGR2HSV)
-# Find the histograms using calcHist.
-M = cv2.calcHist([hsv],[0, 1], None, [180, 256], [0, 180, 0, 256] )
-I = cv2.calcHist([hsvt],[0, 1], None, [180, 256], [0, 180, 0, 256] )
-R = M / I
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector()
 
+# Detect blobs.
+keypoints = detector.detect(im)
 
-h,s,v = cv2.split(hsvt)
-B = R[h.ravel(),s.ravel()]
-B = np.minimum(B,1)
-B = B.reshape(hsvt.shape[:2])
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+img = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-
-# apply a convolution with a circular disc
-disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-cv2.filter2D(B,-1,disc,B)
-B = np.uint8(B)
-cv2.normalize(B,B,0,255,cv2.NORM_MINMAX)
-# apply a convolution with a circular disc
-disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-cv2.filter2D(B,-1,disc,B)
-B = np.uint8(B)
-cv2.normalize(B,B,0,255,cv2.NORM_MINMAX)
-
-cv2.imshow('he', B)
+# Show keypoints
+cv2.imshow("Keypoints", im)
 cv2.waitKey()
